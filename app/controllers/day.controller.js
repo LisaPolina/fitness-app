a21479e57ab9a6a6773dd6491de41b108bf2f350
LisaPolina <51149450+LisaPolina@users.noreply.github.com>
2019-05-29 16:32:41 +0300

    work last

--- app/controllers/day.controller.js
+++ app/controllers/day.controller.js
@@ -1,23 +1,39 @@
 const Day = require('../models/day.model.js');
+const SessionController = require('./session.controller.js');
 
 exports.create = (req, res) => {
     if (!req.body.date) {
         return res.status(400).send({
-            message: "DAte of day cannot be empty."
+            message: "Date of day cannot be empty."
         });
     }
-    
-    // Create a Day
-    const day = new Day({
-        date: req.body.date,
-        total_calories: req.body.total_calories,
-        comment: req.body.comment
-    });
+
+    const tempTotalCalories = parseInt(req.body.total_calories);
+    if (!(tempTotalCalories > 0 && tempTotalCalories < 10000)) {
+        return res.status(400).send({
+            message: "Total number of calories per day must be at range [0, 9999]"
+        });
+    }
 
     // Save Day in the database
-    day.save()
-        .then(data => {
-            res.send(data);
+    SessionController.getUsername(req.cookies.sid)
+        .then(username => {
+            // Create a Day
+            const day = new Day({
+                date: req.body.date,
+                total_calories: tempTotalCalories,
+                comment: req.body.comment,
+                username: username
+            });
+
+            day.save()
+                .then(data => {
+                    res.send(data);
+                }).catch(err => {
+                res.status(500).send({
+                    message: err.message || "Some error occurred while creating the Day."
+                });
+            });
         }).catch(err => {
             res.status(500).send({
                 message: err.message || "Some error occurred while creating the Day."
@@ -26,24 +42,31 @@ exports.create = (req, res) => {
 };
 
 exports.findOne = (req, res) => {
-    Day.findOne({date: req.params.date})
-        .then(day => {
-            if (!day) {
-                return res.status(404).send({
-                    message: "Day not found with date " + req.params.date
+    SessionController.getUsername(req.cookies.sid)
+        .then(username => {
+            Day.findOne({date: req.params.date, username: username })
+                .then(day => {
+                    if (!day) {
+                        return res.status(404).send({
+                            message: "Day not found with date " + req.params.date
+                        });
+                    }
+                    res.send(day);
+                }).catch(err => {
+                if (err.kind === 'ObjectId') {
+                    return res.status(404).send({
+                        message: "Day not found with date " + req.params.date
+                    });
+                }
+                return res.status(500).send({
+                    message: "Error retrieving day"
                 });
-            }
-            res.send(day);
-        }).catch(err => {
-            if (err.kind === 'ObjectId') {
-                return res.status(404).send({
-                    message: "Day not found with date " + req.params.date
-                });
-            }
-            return res.status(500).send({
-                message: "Error retrieving day"
             });
+        }).catch(err => {
+        res.status(500).send({
+            message: err.message || "Error retrieving day"
         });
+    });
 };
 
 exports.update = (req, res) => {
@@ -54,43 +77,58 @@ exports.update = (req, res) => {
         });
     }
 
-    Day.findOne({date: req.params.date})
-        .then(day => {
-            if (!day) {
-                return res.status(404).send({
-                    message: "Day not found with id " + req.params.dayId
-                });
-            } else {
-                if (req.body.comment) {
-                    day.comment = req.body.comment;
-                }
-                if (!req.body.total_calories || parseInt(req.body.total_calories) < 0 || parseInt(req.body.total_calories) > 10000) {
-                    return res.status(404).send({
-                        message: "Total number of calories must be at range [0, 10000]"
-                    });
-                } else {
-                    day.total_calories = parseInt(req.body.total_calories);
-                }
-                day.save()
-                    .then(data => {
-                        res.send(data);
-                    }).catch(err => {
-                    res.status(500).send({
-                        message: err.message || "Some error occurred while saving the Day."
-                    });
-                });
-
-            }
-            res.send(day);
-        }).catch(err => {
-            if (err.kind === 'ObjectId') {
-                return res.status(404).send({
-                    message: "Day not found with date"
-                });
-            }
-            return res.status(500).send({
-                message: "Error updating day"
-            });
+    const tempTotalCalories = parseInt(req.body.total_calories);
+    if (!(tempTotalCalories > 0 && tempTotalCalories < 10000)) {
+        return res.status(400).send({
+            message: "Total number of calories per day must be at range [0, 9999]"
         });
+    }
+
+    SessionController.getUsername(req.cookies.sid)
+        .then(username => {
+            Day.findOne({date: req.params.date, username: username})
+                .then(day => {
+                    if (!day) {
+                        return res.status(404).send({
+                            message: "Day not found with date " + req.params.date
+                        });
+                    } else {
+                        if (req.body.comment) {
+                            day.comment = req.body.comment;
+                        }
+                        if (!req.body.total_calories || parseInt(req.body.total_calories) < 0 || parseInt(req.body.total_calories) > 10000) {
+                            return res.status(404).send({
+                                message: "Total number of calories must be at range [0, 10000]"
+                            });
+                        } else {
+                            day.total_calories = parseInt(req.body.total_calories);
+                        }
+                        day.save()
+                            .then(data => {
+                                res.send(data);
+                            }).catch(err => {
+                            res.status(500).send({
+                                message: err.message || "Some error occurred while saving the Day."
+                            });
+                        });
+
+                    }
+                    res.send(day);
+                }).catch(err => {
+                if (err.kind === 'ObjectId') {
+                    return res.status(404).send({
+                        message: "Day not found with date"
+                    });
+                }
+                return res.status(500).send({
+                    message: "Error updating day"
+                });
+            });
+        }).catch(err => {
+        res.status(500).send({
+            message: err.message || "Error retrieving day"
+        });
+    });
+
 };
 
--- app/models/day.model.js
+++ app/models/day.model.js
@@ -2,12 +2,14 @@ const mongoose = require('mongoose');
 const _ = require('lodash');
 
 const DaySchema = mongoose.Schema({
-    date: { type: String, required: true, unique: true },
+    date: { type: String, required: true},
     username: { type: String, required: true },
     total_calories: {type: Number},
     comment: { type: String },
     }, {
     timestamps: true
 });
+
+DaySchema.index({ date: 1, username: 1}, { unique: true });
 
 module.exports = mongoose.model('Day', DaySchema);
